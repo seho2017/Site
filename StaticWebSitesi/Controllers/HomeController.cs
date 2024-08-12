8f9c0f9293e46e382a8efdab6ce86077c4c4a2d8
seho2017 <149900043+seho2017@users.noreply.github.com>
2024-08-12 14:21:01 +0300

    Add project files.

---
+++ StaticWebSitesi/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWebSitesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("Hakkimizda")]
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        [Route("Ürünler")]
+        public ActionResult Ürünler()
+        {
+            return View();
+        }
+        [Route("Magaza")]
+        public ActionResult Mağaza()
+        {
+            return View();
+        }
+    }
+}
